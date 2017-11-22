b966d826f21c2c23a9efe3c82550feebc14137df
JBYoshi <jbyoshi@users.noreply.github.com>
2017-11-22 13:38:22 -0600

    Implement ConstructPortalEvent for end entrance portals.

--- src/main/java/org/spongepowered/common/mixin/core/item/MixinItemEnderEye.java
+++ src/main/java/org/spongepowered/common/mixin/core/item/MixinItemEnderEye.java
@@ -24,17 +24,28 @@
  */
 package org.spongepowered.common.mixin.core.item;
 
+import com.flowpowered.math.vector.Vector3d;
+import net.minecraft.block.state.pattern.BlockPattern;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemEnderEye;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.projectile.EyeOfEnder;
 import org.spongepowered.api.entity.projectile.source.ProjectileSource;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.world.ConstructPortalEvent;
+import org.spongepowered.api.world.Location;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.common.SpongeImpl;
+import org.spongepowered.common.util.VecHelper;
+
+import javax.annotation.Nullable;
 
 @Mixin(ItemEnderEye.class)
 public class MixinItemEnderEye extends Item {
@@ -45,6 +56,28 @@ public class MixinItemEnderEye extends Item {
         ((EyeOfEnder) enderEye).setShooter((ProjectileSource) player);
         // TODO direct this appropriately
         return world.spawnEntity(enderEye);
+    }
+
+    @Redirect(method = "onItemUse", at = @At(value = "INVOKE", target =
+            "Lnet/minecraft/block/state/pattern/BlockPattern;match(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/state/pattern/BlockPattern$PatternHelper;"))
+    @Nullable
+    private BlockPattern.PatternHelper onConstructPortal(BlockPattern pattern, World worldIn, BlockPos pos) {
+        BlockPattern.PatternHelper match = pattern.match(worldIn, pos);
+        if (match == null) {
+            return null;
+        }
+        Vector3d center = VecHelper.toVector3d(match.getFrontTopLeft())
+                .sub(pattern.getThumbLength() / 2.0, pattern.getFingerLength() / 2.0, pattern.getPalmLength() / 2.0);
+        ConstructPortalEvent event = SpongeEventFactory.createConstructPortalEvent(Sponge.getCauseStackManager().getCurrentCause(),
+                new Location<>((org.spongepowered.api.world.World) worldIn, center));
+        SpongeImpl.postEvent(event);
+        if (event.isCancelled()) {
+            // TODO The eye has already been placed in the portal, so no one
+            // can try to spawn the portal again. Should the interaction be
+            // undone?
+            return null;
+        }
+        return match;
     }
 
 }
